17aee7ef97107e18a83316f406fb729572f5afb4
vinicius <abreu-vinicius15@hotmail.com>
2022-05-11 14:15:04 -0300

    Atividade da aula 1

---
+++ Algoritimos II/Atividade_1/atividade_1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int apt;
+	double litros, l;
+	double total = 0.0;
+	int contador;
+	int ap;
+	
+	printf("Digite a quantidade de apartamentos: \n");
+	scanf("%d",&apt);
+	
+	printf("qtde apatartamentos: %d \n", apt);
+	
+	for (contador = 1; contador<= apt; contador++){
+	printf("Digite a qtde de litros do apartamento: %d \n", contador);
+	scanf("%d", &litros);
+	
+//	l = litros / 1000.0;
+	
+	//printf("qtde litros: %d \n", l);
+	
+	
+	if (contador == 1){
+		total=litros;
+		ap=contador;
+		
+		//printf("contador %d \n", contador);
+		
+		
+	}else{
+		if(litros>total){
+			total = litros;
+			ap=contador;
+		}
+	
+	}
+
+}
+	
+	//printf("O apartamento que mais gastou foi: %d \n", apt);
+	//printf("gastou: %d",total);
+	
+	
+	printf("O apartamento que mais gastou foi: %d \n", ap);
+	printf("E gastou: %d",total / 1000.0);
+	printf(" metros cubicos");
+	
+	return 0;
+}
